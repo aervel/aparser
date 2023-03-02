7c8bc9cd91112c21f72bb198e73f8033984889fd
djafta <djafta.n.m@gmail.com>
2023-03-02 10:26:02 +0200

    feat(JSON): add API to convert objects to JSON

---
+++ src/aervel/aparser/JSON.java
@@ -0,0 +1,15 @@
+package aervel.aparser;
+
+public interface JSON {
+
+    /**
+     * Converts an object to JSON.
+     *
+     * @param object The object to be converted to JSON.
+     * @return A string with JSON representation of underlying object.
+     */
+    static String stringify(Object object) {
+        return null;
+    }
+
+}
