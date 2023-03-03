daf54e8791943f89455375f68092cfdf62739512
djafta <djafta.n.m@gmail.com>
2023-03-04 01:38:10 +0200

    feat(JSON): add implementation to parse lists
    implement method list of Parser to parse lists as literal, objects or lists

--- src/aervel/aparser/json/Parser.java
+++ src/aervel/aparser/json/Parser.java
@@ -1,6 +1,8 @@
 package aervel.aparser.json;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Parser {
@@ -14,7 +16,7 @@ public class Parser {
         carriage.clean();
 
         if (carriage.get() == '[') {
-
+            return list();
         }
 
         if (carriage.get() == '{') {
@@ -26,14 +28,44 @@ public class Parser {
         );
     }
 
+    private List<Object> list() {
+        List<Object> list = new ArrayList<>();
+
+        while (carriage.get() != ']') {
+
+            carriage.next(); // skip the semicolon (:) or bracket ([) characters
+            carriage.clean(); // skip all "\r\n\t " after semicolon or bracket
+
+
+            if ("{[".contains(carriage.get().toString())) {
+                list.add(parse());
+            } else {
+                list.add(literal());
+            }
+
+        }
+
+        if (carriage.hasNext()) {
+            carriage.next(); // skip ] character
+            carriage.clean(); // skip all "\r\n\t " after semicolon
+        }
+
+        return list;
+    }
+
     private Map<String, Object> map() {
         Map<String, Object> map = new HashMap<>();
 
         while (carriage.get() != '}') {
+
+            if ("{,".contains(carriage.get().toString())) {
+                carriage.next(); // skip { or ,
+            }
+
             String key = key();
 
-            carriage.next(); // jump the semicolon (:) character
-            carriage.clean(); // jump all "\r\n\t " after semicolon
+            carriage.next(); // skip the semicolon (:) character
+            carriage.clean(); // skip all "\r\n\t " after semicolon
 
             if ("{[".contains(carriage.get().toString())) {
                 map.put(key, parse());
@@ -42,21 +74,29 @@ public class Parser {
             }
         }
 
+        if (carriage.hasNext()) {
+            carriage.next(); // skip } character
+            carriage.clean(); // skip all "\r\n\t " after semicolon
+        }
+
         return map;
     }
 
     private String literal() {
+
         StringBuilder builder = new StringBuilder();
 
         do {
             builder.append(carriage.get());
         } while (!("}],".contains(carriage.next().toString())));
 
-        return builder.toString();
+        return builder.toString().trim();
     }
 
     String key() {
-        if (carriage.next() != '"') {
+        carriage.clean();
+
+        if (carriage.get() != '"') {
             throw new IllegalArgumentException(
                     "Unexpected token %s at position %d".formatted(carriage.get(), carriage.position())
             );
