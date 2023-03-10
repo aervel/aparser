fef28766b9095c2117900858578411f3625b2279
djafta <djafta.n.m@gmail.com>
2023-03-10 17:07:40 +0200

    feat(serializer): add method to validate a field

--- src/aervel/aparser/json/Fields.java
+++ src/aervel/aparser/json/Fields.java
@@ -1,5 +1,14 @@
 package aervel.aparser.json;
 
+import java.lang.reflect.Field;
+
+import static java.lang.reflect.Modifier.isStatic;
+import static java.lang.reflect.Modifier.isTransient;
+
 public abstract class Fields {
+
+    public static boolean isValid(Field field) {
+        return !(isStatic(field.getModifiers()) || isTransient(field.getModifiers())) && field.trySetAccessible();
+    }
 
 }
