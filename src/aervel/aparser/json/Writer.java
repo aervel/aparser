1cbb19e74dbd01ad647647c9e33712bf61b1538b
djafta <djafta.n.m@gmail.com>
2023-03-11 02:00:20 +0200

    feat(writer): override writer methods to format JSON on write

--- src/aervel/aparser/json/Writer.java
+++ src/aervel/aparser/json/Writer.java
@@ -3,5 +3,33 @@ package aervel.aparser.json;
 import java.io.StringWriter;
 
 public class Writer extends StringWriter {
+    private int tabs;
+    private int last;
 
+    @Override
+    public void write(int c) {
+        if (c == '\n' && (last == '{' || last == '[')) {
+            tabs++;
+        }
+
+        if (last == '\n' && (c == '}' || c == ']')) {
+            tabs--;
+        }
+
+        if (last == '\n') {
+            super.write("\t".repeat(tabs));
+        }
+
+        super.write(c);
+
+        last = c != ' ' ? c : last;
+    }
+
+    @Override
+    public void write(String str) {
+        char[] chars = str.toCharArray();
+        for (char c : chars) {
+            write(c);
+        }
+    }
 }
