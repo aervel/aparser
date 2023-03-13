ca86cd36f7a6e582b0763749c576e222d7c34f80
djafta <djafta.n.m@gmail.com>
2023-03-14 01:12:39 +0200

    feat(reader): add features to support reading process
    add method to check expected characters at specific position skipping formatting characters
    
    add method to check expected characters at next position skipping formatting characters
    
    override method to skip positions without checked exception
    
    add method to get char from current position

--- src/aervel/aparser/json/Reader.java
+++ src/aervel/aparser/json/Reader.java
@@ -5,8 +5,8 @@ import java.io.IOException;
 
 public final class Reader extends java.io.Reader {
     private final StringBuffer buffer;
-    private int column;
-    private int line;
+    private int position = 0;
+    private int[] location = {0, 1};
 
     public Reader(String s) {
         buffer = new StringBuffer(s);
@@ -14,29 +14,47 @@ public final class Reader extends java.io.Reader {
 
     @Override
     public int read() {
-        int read = buffer.charAt(column++);
+
+        if (buffer.length() == position) {
+            return -1;
+        }
+
+        int read = buffer.charAt(position++);
+        location[0]++;
 
         if (read == '\n') {
-            line++;
+            location[1]++;
+            location[0] = 1;
         }
 
         boolean skipSpace = read == '\r' || read == '\t' || read == '\f' || read == '\n';
 
         // skip escaped characters
         while ((skipSpace && read == ' ') || read == '\r' || read == '\t' || read == '\f' || read == '\n') {
-            read = buffer.charAt(column++);
+
+            if (buffer.length() == position) {
+                return -1;
+            }
+
+            read = buffer.charAt(position++);
+            location[0]++;
         }
 
         return read;
     }
 
-    private void clean() {
-
+    @Override
+    public long skip(long n) {
+        try {
+            return super.skip(n);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public int read(char[] cbuf, int off, int len) {
-        int start = column;
+        int start = position;
 
         if ((off < 0) || (off > cbuf.length) || (len < 0) || ((off + len) > cbuf.length) || ((off + len) < 0)) {
             throw new IndexOutOfBoundsException();
@@ -48,11 +66,33 @@ public final class Reader extends java.io.Reader {
             cbuf[off++] = (char) read();
         }
 
-        return start == column ? -1 : column - start;
+        return start == position ? -1 : position - start;
     }
 
     @Override
     public void close() {
 
     }
+
+    public int get() {
+        return buffer.charAt(position > 0 ? position - 1 : position);
+    }
+
+    public void checkNext(char...chars) throws IllegalArgumentException {
+        read();
+        check(chars);
+    }
+
+    public void check(char... chars) throws IllegalArgumentException {
+        int read = position == 0 ? read() : get();
+
+        while (read == ' ') read = read();
+
+        for (char c : chars) {
+            if (read == c) return;
+        }
+
+        throw new IllegalArgumentException("Invalid char at line %d column %d".formatted(location[1], location[0]));
+    }
+
 }
