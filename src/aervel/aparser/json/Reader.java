d3dc3f0608cf40867717b486dfb2ce40a7de5eb5
djafta <djafta.n.m@gmail.com>
2023-03-13 11:00:36 +0200

    feat(reader): make reader to be able to skip formatting characters while reading

--- src/aervel/aparser/json/Reader.java
+++ src/aervel/aparser/json/Reader.java
@@ -15,14 +15,53 @@ public final class Reader extends java.io.Reader {
     public int read() {
         int read = buffer.charAt(column++);
 
+        // resolve escaped special characters
+        if (read == '\\') {
+            switch (read()) {
+
+                case '"' -> {
+                    return '"';
+                }
+
+                case 'r' -> {
+                    return '\r';
+                }
+
+                case 't' -> {
+                    return '\t';
+                }
+
+                case 'f' -> {
+                    return '\f';
+                }
+
+                case 'n' -> {
+                    return '\n';
+                }
+
+                case '\\' -> {
+                    return '\\';
+                }
+
+                default -> throw new IllegalArgumentException();
+            }
+        }
+
         if (read == '\n') {
             line++;
         }
 
+        // skip escaped characters
+        while (read == '\r' || read == '\t' || read == '\f' || read == '\n') {
+            read = buffer.charAt(column++);
+        }
+
+        last = read;
+
         return read;
     }
 
-    private void clear() {
+    private void clean() {
 
     }
 
