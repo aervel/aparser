b856004344bb5b8cdb0923a61f6879a58ffcdb95
djafta <djafta.n.m@gmail.com>
2023-03-14 19:18:45 +0200

    feat(deserializer): add method that its implementation deserialize literals
    the method Deserializer#deserializeLiteral deserialize objects derived from classes in java or javax

--- src/aervel/aparser/json/Deserializer.java
+++ src/aervel/aparser/json/Deserializer.java
@@ -2,8 +2,11 @@ package aervel.aparser.json;
 
 import aervel.aparser.Replacer;
 
-import java.util.Map;
-import java.util.Set;
+import java.lang.reflect.InvocationTargetException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.time.temporal.Temporal;
+import java.util.*;
 
 public abstract class Deserializer {
     private static final Deserializer deserializer = new Deserializer() {
@@ -22,6 +25,24 @@ public abstract class Deserializer {
         return deserializer.deserialize(Wrapper.wrap(new Reader(json)), type);
     }
 
+    private <T> T deserialize(Object object, Class<T> type, Replacer replacer) {
+        String packageName = type.getPackage() == null ? "" : type.getPackageName();
+
+        if (packageName.startsWith("java.") || packageName.startsWith("javax.")) {
+            return deserializeLiteral((String) object, type);
+        }
+
+        if (object instanceof List<?> list && type.isArray()) {
+            Class<?> componentType = type.getComponentType();
+        }
+
+        if (object instanceof Map<?, ?> map) {
+
+        }
+
+        throw new IllegalArgumentException();
+    }
+
     private <T> T deserialize(Object wrap, Class<T> type, String[] replacer) {
         // Use a Set because is most flexible to query for elements
         Set<String> set = Set.of(replacer);
@@ -31,10 +52,6 @@ public abstract class Deserializer {
         return deserialize(wrap, type, replacer0);
     }
 
-    private <T> T deserialize(Object wrap, Class<T> type, Replacer replacer) {
-        throw new IllegalArgumentException();
-    }
-
     private <T> T deserialize(Object wrap, Class<T> type) {
         // Create a replacer instance that return the entries for all (key, value) in object
         Replacer replacer0 = (Map::entry);
@@ -42,4 +59,49 @@ public abstract class Deserializer {
         return deserialize(wrap, type, replacer0);
     }
 
+    @SuppressWarnings("unchecked")
+    private <T> T deserializeLiteral(String object, Class<?> type) {
+        try {
+            if (type.isPrimitive()) {
+                if (type.equals(byte.class)) type = Byte.class;
+                else if (type.equals(short.class)) type = Short.class;
+                else if (type.equals(char.class)) type = Character.class;
+                else if (type.equals(int.class)) type = Integer.class;
+                else if (type.equals(long.class)) type = Long.class;
+                else if (type.equals(float.class)) type = Float.class;
+                else if (type.equals(double.class)) type = Double.class;
+                else if (type.equals(boolean.class)) type = Boolean.class;
+            }
+
+            if (Object.class.equals(type)) {
+                return (T) object;
+            }
+
+            if (Date.class.isAssignableFrom(type)) {
+                return (T) DateFormat.getInstance().parse(object);
+            }
+
+            if (String.class.isAssignableFrom(type)) {
+                return (T) object;
+            }
+
+            if (Temporal.class.isAssignableFrom(type)) {
+                return (T) type.getDeclaredMethod("parse", CharSequence.class).invoke(null, object);
+            }
+
+            if (Character.class.isAssignableFrom(type)) {
+                return (T) Character.valueOf(object.charAt(0));
+            }
+
+            if (Number.class.isAssignableFrom(type) || Boolean.class.isAssignableFrom(type)) {
+                return (T) type.getDeclaredMethod("valueOf", String.class).invoke(null, object);
+            }
+
+            throw new IllegalArgumentException();
+
+        } catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException | ParseException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
+
