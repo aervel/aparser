e2c33beb2664be1621fc8f5e440699ce0b2d2725
djafta <djafta.n.m@gmail.com>
2023-03-14 17:00:07 +0200

    feat(deserializer): add implementation of Deserializer#deserialize method that don't receive replacer argument
    the implementation forwards the responsibility of deserialization

--- src/aervel/aparser/json/Deserializer.java
+++ src/aervel/aparser/json/Deserializer.java
@@ -1,11 +1,28 @@
 package aervel.aparser.json;
 
+import aervel.aparser.Replacer;
+
+import java.util.Map;
+
 public abstract class Deserializer {
-    private static Deserializer deserializer = new Deserializer() {
+    private static final Deserializer deserializer = new Deserializer() {
     };
 
     public static <T> T deserialize(String json, Class<T> type, Object replacer) {
+
+        return deserializer.deserialize(Wrapper.wrap(new Reader(json)), type);
+    }
+
+    private <T> T deserialize(Object wrap, Class<T> type, Replacer replacer) {
         throw new IllegalArgumentException();
+    }
+
+
+    private <T> T deserialize(Object wrap, Class<T> type) {
+        // Create a replacer instance that return the entries for all (key, value) in object
+        Replacer replacer0 = (Map::entry);
+        // Forward the responsibility of deserialization to a method that uses an instance of Replacer
+        return deserialize(wrap, type, replacer0);
     }
 
 }
