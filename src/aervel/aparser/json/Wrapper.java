61b1fb8abf1fb118bb901a4bc0658548311c287a
djafta <djafta.n.m@gmail.com>
2023-03-16 07:02:50 +0200

    fix(wrapper): add missing "[" character to expected array of arguments
    the method checkNext throws an exception when wrapping a multidimensional JSON array because it wasn't expecting for an opening character of array

--- src/aervel/aparser/json/Wrapper.java
+++ src/aervel/aparser/json/Wrapper.java
@@ -37,7 +37,7 @@ public class Wrapper {
             List<Object> list = new ArrayList<>();
 
             while (reader.get() != ']') {
-                reader.checkNext('{', ']', '"', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9');
+                reader.checkNext('{', ']', '[', '"', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9');
 
                 switch (reader.get()) {
                     case '{', '[' -> list.add(wrap(reader));
