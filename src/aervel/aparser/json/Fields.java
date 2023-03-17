a9156ab177cb2e82a457e6c3cbd5afd06a7b9657
djafta <djafta.n.m@gmail.com>
2023-03-17 08:56:01 +0200

    refactor(deserializer): add throws declaration to method signature

--- src/aervel/aparser/json/Fields.java
+++ src/aervel/aparser/json/Fields.java
@@ -22,10 +22,12 @@ public abstract class Fields {
      *
      * @param field The field to be validated.
      * @return True if the field is valid or false otherwise.
+     * @throws NullPointerException If field references to null.
      */
-    public static boolean isValid(Field field) {
+    public static boolean isValid(Field field) throws NullPointerException {
         return !(isStatic(field.getModifiers()) || isTransient(field.getModifiers())) && field.trySetAccessible();
     }
+
 
     public static Field[] of(Class<?> cls) {
         return Arrays.stream(cls.getDeclaredFields()).filter(Fields::isValid).toArray(Field[]::new);
