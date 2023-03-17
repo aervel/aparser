db32ff9d272622f488cfa8d3fc6b26ce41243706
djafta <djafta.n.m@gmail.com>
2023-03-17 08:46:51 +0200

    docs(deserializer): add javadoc to Constructors#of method and update signature
    add throws for expected exceptions and documentation to method

--- src/aervel/aparser/json/Constructors.java
+++ src/aervel/aparser/json/Constructors.java
@@ -6,12 +6,29 @@ import java.util.Arrays;
 
 public class Constructors {
 
+    /**
+     * Uses the fields from same class to create and return a constructor. The returned constructor has the same type
+     * of parameters in the same order with fields arguments. Those fields must be valid:
+     * <ul>
+     *     <li>Non-static</li>
+     *     <li>Non-transient</li>
+     *     <li>Accessible for reflection</li>
+     * </ul>
+     * <p>
+     * The returned constructor is accessible, else an exception is thrown.
+     *
+     * @param fields The valid fields of class from where the constructor is expected.
+     * @param <T>    The type of constructor.
+     * @return A constructor which parameter types matches with fields.
+     * @throws IllegalArgumentException If fields doesn't have at least a field or if constructor is inaccessible.
+     * @throws RuntimeException         If constructor wasn't found.
+     */
     @SuppressWarnings("unchecked")
-    public static <T> Constructor<T> of(Field... fields) {
+    public static <T> Constructor<T> of(Field... fields) throws IllegalArgumentException, RuntimeException {
         try {
 
             if (fields.length == 0) {
-                throw new IllegalArgumentException();
+                throw new IllegalArgumentException("fields.length == 0");
             }
 
             Class<T> cls = (Class<T>) fields[0].getDeclaringClass();
